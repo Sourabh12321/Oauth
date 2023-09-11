f2d06756ca53b881c9e76d84b945cfea2d369b96
unknown <sourabh.rajput.22082001@gmail.com>
2023-09-11 15:18:41 +0530

    complete the Oauth

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ require("dotenv").config();
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:8000/auth/google/callback"
+  callbackURL: "https://oauth-coding-query.onrender.com/auth/google/callback"
 },
   async function (accessToken, refreshToken, profile, cb) {
     let name = profile._json.name;
